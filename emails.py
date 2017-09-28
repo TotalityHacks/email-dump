c65a0fc362444e9cbf40380706519da9f793add6
yohanstra <jreinstra@gmail.com>
2017-09-28 14:07:07 -0700

    added basic connection thing

--- emails.py
+++ emails.py
@@ -1,8 +1,11 @@
 from flask import Flask, request
+from flask.ext.sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 CORS(app)
 
 
